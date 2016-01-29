e5d9a7f09a2a27f725ec20ad29fb4e3d40d1ba94
Garett Dunn <garettmd@gmail.com>
2016-01-29 20:22:36 +0000

    Added logging flag to allow for smaller log messages

--- photomove.py
+++ photomove.py
@@ -33,11 +33,12 @@ logger.addHandler(handler)
 # Configure argparser
 parser = argparse.ArgumentParser(description=DESCRIPTION)
 parser.add_argument('-s', '--source', help='source directory', required=True)
-parser.add_argument(
-    '-d', '--destination', help='destination directory', required=True)
+parser.add_argument('-d', '--destination', help='destination directory', required=True)
+parser.add_argument('-l', '--loglevel', help='loglevel: Turn flag on for extra error info', required=False, action='store_true')
 args = parser.parse_args()
 srcdir = args.source
 destdir = args.destination
+logging = args.loglevel
 #####################
 
 
@@ -50,20 +51,20 @@ def move(src, dest):
             shutil.move(src, dest)
     except:
         logger.error('Failed to move file %s to %s' %
-                     (src, dest), exc_info=True)
+                     (src, dest), exc_info=logging)
     else:
         logger.info("File %s successfully moved to %s" % (src, dest))
 
 
 def main():
-    logger.info('\n\nStarting main function')
+    logger.info('\n\nStarting main function\n\n')
     dirs = srcdir
     for filename in listdir(dirs):
         logger.debug('\nFile: %s \t Type: %s' % (filename, guess_type(filename)))
         try:
             data = Metadata(srcdir + filename)
         except:
-            logger.error("%s: Filetype not supported" % filename, exc_info=True)
+            logger.error("%s: Filetype not supported" % filename, exc_info=logging)
             move(srcdir + filename, EXIFERRORDIR)
         else:
             # Get EXIF data from image
@@ -71,7 +72,7 @@ def main():
                 date = data.get_date_time()
             except:
                 logger.error("%s: No date-time specified in file. Leaving in current directory" %
-                    filename, exc_info=True)
+                    filename, exc_info=logging)
             else:
                 year = date.strftime("%Y")
                 month = date.strftime("%m")
